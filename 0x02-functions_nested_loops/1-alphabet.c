0c18ffae3ee50a85764629ee1eb0f7f94c987645
Ompheletse Makwela <omphentse@gmail.com>
2023-09-12 09:18:57 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include "main.h"
+
+/**
+ * Main - Entry point
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+}
+int man(void)
+{
+	print_alphabet();
+	return (0);
+}
