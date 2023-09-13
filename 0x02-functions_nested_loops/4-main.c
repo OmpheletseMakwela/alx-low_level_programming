78b711b5cc571cd8de9268fd81f9641e40dd4811
Ompheletse Makwela <omphentse@gmail.com>
2023-09-13 03:07:05 -0700

    isalpha

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,24 +1,28 @@
 #include "main.h"
-# include <ctype.h>
-# include <stdio.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * main - check the code.
+ * test_isalpha - Test the _isalpha function
+ *
+ * @n: Number to pass to _isalpha function
+ */
+void test_isalpha(int n)
+{
+	int r;
+
+	r = isalpha(n);
+	putchar(r + '0');
+	putchar('\n');
+}
+
+/**
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int r;
-
-    r = isalpha('H');
-    putchar(r + '0');
-    r = isalpha('o');
-    putchar(r + '0');
-    r = isalpha(108);
-    putchar(r + '0');
-    r = isalpha(';');
-    putchar(r + '0');
-    putchar('\n');
-    return (0);
+	test_isalpha('H');
+	return (0);
 }
