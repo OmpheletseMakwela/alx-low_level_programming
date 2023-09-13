065f0b5ce74769c5b776e56599818ca4c0c34186
Ompheletse Makwela <omphentse@gmail.com>
2023-09-13 02:33:20 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -17,23 +17,4 @@ int _islower(int c)
 		return 0;
 	}
 }
-/**
- * main_islower - Entry point of function
- *
- * return:0 (success)
- */
-int main_islower(void)
-{
-	int i;
 
-	i = _islower('a');
-	if (i == 1)
-	{
-		putchar(1);
-	}
-	else
-	{
-		putchar(0);
-	}
-	return (0);
-}
