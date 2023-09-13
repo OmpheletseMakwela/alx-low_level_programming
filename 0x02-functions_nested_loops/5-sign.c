1635a4a153f8ca60fff253728959372d467c51bf
Ompheletse Makwela <omphentse@gmail.com>
2023-09-13 11:59:46 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,11 @@
 # include <stdio.h>
 
 /**
+ * print_sign - prototype function
  * print_sign - prototype function that checks for the sign of the character
  * @n: The number to check
  *
- * return: 1 if is greater than zero,0 if n is zero and -1 if n is less than 0 
+ * return: 1 if is greater than zero,0 if n is zero and -1 if n is less than 0
  */
 int print_sign(int n)
 {
