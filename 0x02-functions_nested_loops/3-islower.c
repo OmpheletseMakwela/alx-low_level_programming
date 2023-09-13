ea123c0117374f1efd221fd0a3fb964b5117fb48
Ompheletse Makwela <omphentse@gmail.com>
2023-09-13 12:22:28 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,10 @@
 /**
  * _islower - A prototype of a function that checks for a lowercase character
  *
+ * c is a variable declared
+ *
+ * _islower is the prototype function
+ *
  * Return : 0 Always success
  */
 int _islower(int c)
