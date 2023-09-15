03055729c3850dc5dfa1ed1d70b7f8bbf811703a
Ompheletse Makwela <omphentse@gmail.com>
2023-09-15 12:02:36 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+# include "main.h"
+
+/**
+ * int mul - multiplies two digits
+ * int a and int b are integers declared
+ * return 0 always a success
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
