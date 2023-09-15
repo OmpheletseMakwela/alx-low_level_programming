adf943358bdd75ee38b09747da83adb7119ce005
Ompheletse Makwela <omphentse@gmail.com>
2023-09-15 06:05:29 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,12 +4,11 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	/**
 	 * i = 0;
@@ -18,8 +17,6 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
