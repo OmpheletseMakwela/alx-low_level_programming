bc4b047597ac1abc71be676feab2ddccd846f58c
Ompheletse Makwela <omphentse@gmail.com>
2023-09-18 11:32:14 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+# include "main.h"
+# include <string.h>
+
+/**
+ * _strlen - A prototype function that calculates the length of the string
+ * @s: a pointer that contains a string
+ * return 0 always a success
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
