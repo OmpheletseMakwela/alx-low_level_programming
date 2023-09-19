0ded6aaa9613ec6dd1b71cad22d45555d09ca1fc
Ompheletse Makwela <omphentse@gmail.com>
2023-09-19 12:47:15 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+# include "main.h"
+
+/**
+ * puts2 - prints every character on even position
+ * @str: Is a pointer that is assigned
+ * @position: ia an integer position
+ */
+void puts2(char *str)
+{
+	int position = 0;
+
+	while (str[position] != '\0')
+	{
+		if (position % 2 == 0)
+		{
+			_putchar(str[position]);
+		}
+		position++;
+	}
+}
