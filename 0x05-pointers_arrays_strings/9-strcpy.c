b5982a182b8e53eeaa06368b85d96cb00880a059
Ompheletse Makwela <omphentse@gmail.com>
2023-09-19 15:02:58 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,9 @@
 # include <stdio.h>
 
 /**
- * *_strcpy - A prototype function that copies a string
- * @*dest: A pointer that is assigned
- * @*src: A pointer that is assigned
+ * _strcpy - A prototype function that copies a string
+ * @dest: A pointer that is assigned
+ * @src: A pointer that is assigned
  */
 char *_strcpy(char *dest, char *src)
 {
