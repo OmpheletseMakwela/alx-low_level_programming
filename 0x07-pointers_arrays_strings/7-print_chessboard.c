5bbb58157fd3b49a4b22c33d1e0edfa72832baa6
Ompheletse Makwela <omphentse@gmail.com>
2023-09-24 14:29:58 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - A function that prints a chessboard
+ * @a: A pointer
+ * Return: chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+	{
+		printf("%c", a[i][j]);
+	}
+	printf("\n");
+	}
+}
