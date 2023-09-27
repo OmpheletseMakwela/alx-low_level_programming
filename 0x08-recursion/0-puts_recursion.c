af2d32d9766dcbfd01dff7d72a2ecf8b8a62fa9a
Ompheletse Makwela <omphentse@gmail.com>
2023-09-26 22:37:13 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - A prototype that prints a string followed by a new line
+ * @s: A pointer that hold an adress of a string
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+	printf("\n");
+}
+
