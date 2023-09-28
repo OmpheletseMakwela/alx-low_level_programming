c9a53fcab6653c371e46ce0a416cd12b3ce40f16
Ompheletse Makwela <omphentse@gmail.com>
2023-09-27 17:10:34 -0700

    You must not be afraid to dream a little bigger,darling

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - A prototype that returns the length of a string
+ * @s: A pointer that contains the address of a pointer
+ * Return: the length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	int r;
+
+	r = strlen(s);
+	return (r);
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ */ 
+int factorial(int n)
+{
+	int res;
+	res = 1;
+
+	if (n < 0)
+		return (-1);
+	else
+		res = res * n - 1;
+	return (res);
+}
