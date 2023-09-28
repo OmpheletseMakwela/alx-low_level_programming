af57aff8bf8e112eaffbf24b2d45860def999012
Ompheletse Makwela <omphentse@gmail.com>
2023-09-27 17:31:58 -0700

    You must not be afraid to dream a little bigger,darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- */ 
+ * factorial - Calculates a factorial of a number
+ * Return: 1 if equal to 0; -1 if less than 0 and rest if else
+ */
 int factorial(int n)
 {
 	int res;
-	res = 1;
 
+	res = 1;
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
@@ -15,8 +17,11 @@ int factorial(int n)
 	}
 	else
 	{
-		res = res * n - 1;
+		while (n > 0)
+		{
+			res *= n;
+			n--;
+		}
 		return (res);
 	}
-		
 }
