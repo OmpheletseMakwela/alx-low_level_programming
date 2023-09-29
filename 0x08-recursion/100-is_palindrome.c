77b443a31386f1298408eda55b5cdd49a023f5d1
Ompheletse Makwela <omphentse@gmail.com>
2023-09-28 17:32:47 -0700

    palindome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,40 +1,37 @@
 #include "main.h"
 #include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 
-int is_palindrome_recursive(char *s, int start, int end)
-{
-    if (start >= end)
-    {
-        return 1; // Base case: It's a palindrome
-    }
+/**
+ * is_substring_palindrome - A function
+ * is_palindrome - A function
+ * @s: variable
+ * @start: variable
+ * @end: variable
+ * Return: 1 always a success
+ */
 
-    while (!isalnum(s[start]) && start < end)
-    {
-        start++;
-    }
-
-    while (!isalnum(s[end]) && start < end)
-    {
-        end--;
-    }
-
-    if (tolower(s[start]) != tolower(s[end]))
-    {
-        return 0; // Not a palindrome
-    }
-
-    return is_palindrome_recursive(s, start + 1, end - 1);
-}
-
+int is_substring_palindrome(char *s, int start, int end);
+/**
+ * is_palindome - A function
+ */
 int is_palindrome(char *s)
 {
-    int length = strlen(s);
+	int length = strlen(s);
 
-    if (length == 0)
-    {
-        return 1;
-    }
+	return (is_substring_palindrome(s, 0, length - 1));
+}
 
-    return is_palindrome_recursive(s, 0, length - 1);
+int is_substring_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	return (is_substring_palindrome(s, start + 1, end - 1));
 }
