76973fba32e045587b037ea035e467df3b88ea49
Ompheletse Makwela <omphentse@gmail.com>
2023-10-10 02:18:55 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	printf("name = %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("age = %f\n", (d->age));
-	printf("onwer = %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	printf("name : %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("age : %f\n", (d->age));
+	printf("onwer : %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
