c017dc81ef933afe1e2f365ce93275201c23066a
Ompheletse Makwela <omphentse@gmail.com>
2023-10-09 23:06:38 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - group together variable of different data types
+ * @name: name
+ * @age:age
+ * @owner:owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
