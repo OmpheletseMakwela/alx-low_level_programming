e1c01160d257c203759507d3376e42f0b489de3f
Ompheletse Makwela <omphentse@gmail.com>
2023-10-10 04:52:59 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,12 @@
 #define MAIN_H
 
 struct dog {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+typedef struct dog dog_t;
 
 #endif /* MAIN_H */
