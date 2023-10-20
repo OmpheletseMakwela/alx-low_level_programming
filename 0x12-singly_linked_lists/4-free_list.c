95ef873d9be9742e74e8812e1d775cec0fa423e9
Ompheletse Makwela <omphentse@gmail.com>
2023-10-19 19:22:37 -0700

    new node

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "list.h"
+
+void free_list(list_t *head) {
+    list_t *current = head;
+    list_t *next;
+
+    while (current != NULL) {
+        next = current->next;
+        free(current->str);
+        free(current);
+        current = next;
+    }
+}
