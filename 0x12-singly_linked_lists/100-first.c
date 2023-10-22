9f24d6a5bec64dcb905f9bc9d49a772485dac1d6
Ompheletse Makwela <omphentse@gmail.com>
2023-10-22 06:49:04 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * my_startup_function - prints a string
+ */
 void my_startup_function(void) __attribute__ ((constructor));
 
-void my_startup_function(void) {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+void my_startup_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
