7777802854e36b240813ce5565930138fb9f17c2
Ompheletse Makwela <omphentse@gmail.com>
2023-10-26 16:27:32 -0700

    task 1

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Custom function to print a character to stdout.
+ * @c: The character to print.
+ *
+ * Return: On success, 1. On error, -1.
+ */
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
