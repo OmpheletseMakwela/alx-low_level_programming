61a02424e9cf76ffe61b6117b839f89ba9fa1fd0
Ompheletse Makwela <omphentse@gmail.com>
2023-10-30 19:37:48 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * create_file - Create a file with specified permissions and content.
+ * @filename: The name of the file to create.
+ * @text_content: The text content to write to the file
+ * Return: 1 on success, -1 on failure
+ * If filename is NULL, return -1.
+ * If text_content is NULL, create an empty file.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor, write_result;
+
+	if (filename == NULL)
+		return (-1);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IWUSR);
+	if (file_descriptor == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		write_result = write(file_descriptor, text_content, strlen(text_content));
+		if (write_result == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+	close(file_descriptor);
+	return (1);
+}
