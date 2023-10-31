463b9f474abe9a0f328cbc4f2f60e18f512be7b2
Ompheletse Makwela <omphentse@gmail.com>
2023-10-30 19:40:37 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IWUSR);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_descriptor == -1)
 		return (-1);
 	if (text_content != NULL)
