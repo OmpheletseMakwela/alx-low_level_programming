3ce695be3ca1266085a24a4a28839aabaa803b4f
Ompheletse Makwela <omphentse@gmail.com>
2023-12-22 23:47:38 +0200

    task1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,12 +8,12 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash = 5381;
-    int a;
-    
-    while ((a = *str++)) {
-        hash = ((hash << 5) + hash) + a;
-    }
+	unsigned long int hash = 5381;
+	int a;
 
-    return (hash);
+	while ((a = *str++))
+	{
+		hash = ((hash << 5) + hash) + a;
+	}
+	return (hash);
 }
