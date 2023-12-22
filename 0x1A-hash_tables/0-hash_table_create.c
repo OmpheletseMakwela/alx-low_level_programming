8f75ad0c1aa4886372448b9650de51aebf24cd53
Ompheletse Makwela <omphentse@gmail.com>
2023-12-23 00:45:41 +0200

    adv

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = NULL;
-	unsigned long int i;
-
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (hash_table->array == NULL)
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_node_t *));
+	if (ht->array == NULL)
 	{
-		free(hash_table);
+		free(ht);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	for (unsigned long int i = 0; i < size; i++)
 	{
-		hash_table->array[i] = NULL;
+		ht->array[i] = NULL;
 	}
-	hash_table->size = size;
-	return (hash_table);
+	return (ht);
 }
