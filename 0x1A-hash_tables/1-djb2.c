699c6b6f95d2ab9140fd877a89f016467f0f0949
Ompheletse Makwela <omphentse@gmail.com>
2023-12-22 23:45:50 +0200

    task1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,17 +2,17 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Hashes a string using the djb2 algorithm.
+ * hash_djb2 - Hashes a string with djb2 algorithm.
  * @str: The string to be hashed.
- *
- * Return: The hash value generated from the input string.
+ * Return: The hash value.
  */
-unsigned long int hash_djb2(const unsigned char *str) {
+unsigned long int hash_djb2(const unsigned char *str)
+{
     unsigned long int hash = 5381;
-    int c;
+    int a;
     
-    while ((c = *str++)) {
-        hash = ((hash << 5) + hash) ^ c;
+    while ((a = *str++)) {
+        hash = ((hash << 5) + hash) + a;
     }
 
     return (hash);
